5a8b7adb9127ee34e5412b7499b24a039dfdde5d
Chinmay Pradhan <32996653+Chinmay2911@users.noreply.github.com>
2019-04-16 17:09:54 +0530

    Delete division.py

--- division.py
+++
@@ -1,8 +0,0 @@
-a=int(input("Enter the first number: "))
-b=int(input("Enter the second number: "))
-
-quotient=a/b
-remainder=a%b
-
-print("Quotient is:",quotient)
-print("Remainder is:",remainder)
